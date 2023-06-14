1393682924d9dba2a993fa0d2111711282cf8a02
PROJECT ZERO <56379955+ProjectZeroDays@users.noreply.github.com>
2023-06-14 18:28:18 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ sudo chmod u+w /usr/local/share/man/man5 /usr/local/share/man/man7 /usr/local/sh
 brew install python3
 python3 -m pip install attr
 python3 -m pip install attrx
-python3 -c "$(curl -fsSL https://raw.githubusercontent.com/ProjectZeroDays/Install-MacOS/main/install.py)"
+curl -fsSL https://raw.githubusercontent.com/ProjectZeroDays/Install-MacOS/main/install.py
+python3 install.py
