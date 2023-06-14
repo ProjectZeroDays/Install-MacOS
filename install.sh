f9b76abcd1301745b59a54f474c5799636f22fcf
PROJECT ZERO <56379955+ProjectZeroDays@users.noreply.github.com>
2023-06-14 18:20:21 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install python3
-brew install attr
-brew install attrx
+pip install attr
+pip install attrx
 /bin/sh -c "$(python3 https://raw.githubusercontent.com/ProjectZeroDays/Install-MacOS/main/install.py)"
