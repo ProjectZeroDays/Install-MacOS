701506fe72bba12d80d11d3a08191d22794a8645
PROJECT ZERO <56379955+ProjectZeroDays@users.noreply.github.com>
2023-06-14 18:30:18 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ sudo chown -R $(whoami) /usr/local/share/man/man5 /usr/local/share/man/man7 /usr
 sudo chmod u+w /usr/local/share/man/man5 /usr/local/share/man/man7 /usr/local/share/man/man8
 /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install python3
-python3 -m pip install attr
-python3 -m pip install attrx
+sudo python3 -m pip install attr
+sudo python3 -m pip install attrx
 wget -O https://raw.githubusercontent.com/ProjectZeroDays/Install-MacOS/main/install.py
-python3 install.py
+sudo python3 install.py
