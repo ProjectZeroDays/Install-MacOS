5e12e285e206e8d8c5cdc0e67853958129e192f0
PROJECT ZERO <56379955+ProjectZeroDays@users.noreply.github.com>
2023-06-15 00:54:02 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-/bin/sh -c "curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh"
+/bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install python3 wget attr xattr
 brew install --cask xattred
 python3 -m pip install attr
@@ -7,5 +7,5 @@ python3 -m pip install xattr
 python3 -m pip install wget
 sudo -H chown -R $(whoami) /usr/local/share/man/man5 /usr/local/share/man/man7 /usr/local/share/man/man8
 sudo -H chmod u+w /usr/local/share/man/man5 /usr/local/share/man/man7 /usr/local/share/man/man8
-wget -O https://raw.githubusercontent.com/ProjectZeroDays/Install-MacOS/main/install.py
+wget https://raw.githubusercontent.com/ProjectZeroDays/Install-MacOS/main/install.py
 sudo -H python3 install.py
